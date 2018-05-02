8484b4783d189dd98a582c4cf968cecd9025a744
Danny Berger <dpb587@gmail.com>
2018-05-02 04:43:56 -0700

    fly autologin/sync

--- scheduler/concourse/runner.go
+++ scheduler/concourse/runner.go
@@ -26,6 +26,7 @@ type Runner struct {
 	SecretsPath string
 
 	needsLogin bool
+	needsSync  bool
 }
 
 func (c *Runner) Schedule(t task.Task) error {
@@ -53,11 +54,6 @@ func (c *Runner) Schedule(t task.Task) error {
 		return fmt.Errorf("writing pipeline: %v", err)
 	}
 
-	err = c.login()
-	if err != nil {
-		return fmt.Errorf("logging in: %v", err)
-	}
-
 	_, _, err = c.runStdin(
 		bytes.NewBufferString("y\n"),
 		"set-pipeline",
@@ -82,11 +78,6 @@ func (c *Runner) Schedule(t task.Task) error {
 
 func (c *Runner) Status(t task.Task) (scheduler.Status, error) {
 	pipelineName := c.pipelineName(t)
-
-	err := c.login()
-	if err != nil {
-		return scheduler.StatusUnknown, fmt.Errorf("logging in: %v", err)
-	}
 
 	stdout, stderr, err := c.run("jobs", "--pipeline", pipelineName)
 	if err != nil {
@@ -126,45 +117,78 @@ func (c *Runner) Status(t task.Task) (scheduler.Status, error) {
 	return scheduler.StatusUnknown, errors.New("unrecognized pipeline state")
 }
 
-func (c *Runner) login() error {
-	if !c.needsLogin {
-		return nil
+func (c *Runner) prepare() error {
+	if c.needsLogin {
+		args := []string{
+			"-c", c.URL,
+			"-n", c.Team,
+			"-u", c.Username,
+			"-p", c.Password,
+		}
+
+		if c.Insecure {
+			args = append(args, "-k")
+		}
+
+		_, _, err := c.run("login", args...)
+		if err != nil {
+			return fmt.Errorf("logging in: %v", err)
+		}
+
+		c.needsLogin = false
 	}
 
-	args := []string{
-		"login",
-		"-c", c.URL,
-		"-n", c.Team,
-		"-u", c.Username,
-		"-p", c.Password,
-	}
+	if c.needsSync {
+		args := []string{
+			"-c", c.URL,
+			"-n", c.Team,
+			"-u", c.Username,
+			"-p", c.Password,
+		}
 
-	if c.Insecure {
-		args = append(args, "-k")
-	}
+		if c.Insecure {
+			args = append(args, "-k")
+		}
 
-	_, _, err := c.run(args...)
-	if err != nil {
-		return fmt.Errorf("logging in: %v", err)
-	}
+		_, _, err := c.run("sync", args...)
+		if err != nil {
+			return fmt.Errorf("syncing: %v", err)
+		}
 
-	c.needsLogin = false
+		c.needsSync = false
+	}
 
 	return nil
 }
 
-func (c *Runner) runStdin(stdin io.Reader, args ...string) ([]byte, []byte, error) {
-	allArgs := append([]string{"-t", c.Target}, args...)
+func (c *Runner) isPrepareCommand(command string) bool {
+	return command == "login" || command == "sync"
+}
+
+func (c *Runner) runStdin(stdin io.Reader, command string, args ...string) ([]byte, []byte, error) {
+	if !c.isPrepareCommand(command) {
+		err := c.prepare()
+		if err != nil {
+			return nil, nil, fmt.Errorf("preparing to run: %v", err)
+		}
+	}
+
+	allArgs := append([]string{"-t", c.Target, command}, args...)
 	cmd := exec.Command("/usr/local/bin/fly", allArgs...)
 
 	outbuf := bytes.NewBuffer(nil)
 	errbuf := bytes.NewBuffer(nil)
 
-	cmd.Stdin = stdin
+	stdinAll, err := ioutil.ReadAll(stdin)
+	if err != nil {
+		return nil, nil, fmt.Errorf("buffering stdin: %v", err)
+	}
+
+	cmd.Stdin = bytes.NewBuffer(stdinAll)
 	cmd.Stdout = outbuf
 	cmd.Stderr = errbuf
 
-	err := cmd.Start()
+	err = cmd.Start()
 	if err != nil {
 		return nil, nil, fmt.Errorf("starting command: %v", err)
 	}
@@ -177,9 +201,13 @@ func (c *Runner) runStdin(stdin io.Reader, args ...string) ([]byte, []byte, erro
 
 		if strings.Contains(string(errbuf.Bytes()), "not authorized.") {
 			c.needsLogin = true
+		} else if strings.Contains(string(errbuf.Bytes()), "out of sync with the target") {
+			c.needsSync = true
 		}
 
-		// TODO fly sync "out of sync with the target"
+		if !c.isPrepareCommand(command) {
+			return c.runStdin(bytes.NewBuffer(stdinAll), command, args...)
+		}
 
 		return outbuf.Bytes(), errbuf.Bytes(), fmt.Errorf("cli: running %#+v: %v", allArgs, err)
 	}
@@ -187,8 +215,8 @@ func (c *Runner) runStdin(stdin io.Reader, args ...string) ([]byte, []byte, erro
 	return outbuf.Bytes(), errbuf.Bytes(), nil
 }
 
-func (c *Runner) run(args ...string) ([]byte, []byte, error) {
-	return c.runStdin(bytes.NewBuffer(nil), args...)
+func (c *Runner) run(cmd string, args ...string) ([]byte, []byte, error) {
+	return c.runStdin(bytes.NewBuffer(nil), cmd, args...)
 }
 
 func (c *Runner) pipelineName(t task.Task) string {
