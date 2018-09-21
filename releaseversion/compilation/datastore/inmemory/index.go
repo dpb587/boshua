a89033c3791e0e9faff8a11ccd164de345bb1cab
Danny Berger <dpb587@gmail.com>
2018-09-21 11:28:09 +0000

    add inmemory compilation datastore

---
+++ releaseversion/compilation/datastore/inmemory/index.go
@@ -0,0 +1,56 @@
+package inmemory
+
+import (
+	"github.com/dpb587/boshua/releaseversion/compilation"
+	"github.com/dpb587/boshua/releaseversion/compilation/datastore"
+)
+
+type Index struct {
+	artifacts []compilation.Artifact
+}
+
+var _ datastore.Index = &Index{}
+
+func New() *Index {
+	return &Index{}
+}
+
+func (Index) GetName() string {
+	panic("not supported directly")
+}
+
+func (i *Index) Add(artifact compilation.Artifact) {
+	i.artifacts = append(i.artifacts, artifact)
+}
+
+func (i *Index) GetCompilationArtifacts(f datastore.FilterParams) ([]compilation.Artifact, error) {
+	var results = []compilation.Artifact{}
+
+	for _, artifact := range i.artifacts {
+		if !f.OS.NameSatisfied(artifact.OS.Name) {
+			continue
+		} else if !f.OS.VersionSatisfied(artifact.OS.Version) {
+			continue
+		} else if !f.Release.ChecksumSatisfied(artifact.MetalinkFile().Hashes) {
+			continue
+		} else if !f.Release.NameSatisfied(artifact.Release.Name) {
+			continue
+		} else if !f.Release.VersionSatisfied(artifact.Release.Version) {
+			continue
+		}
+
+		results = append(results, artifact)
+	}
+
+	return results, nil
+}
+
+func (i *Index) FlushCompilationCache() error {
+	i.artifacts = []compilation.Artifact{}
+
+	return nil
+}
+
+func (i *Index) StoreCompilationArtifact(artifact compilation.Artifact) error {
+	return datastore.UnsupportedOperationErr
+}
